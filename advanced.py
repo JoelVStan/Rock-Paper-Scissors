5dc9c92b9b18c9009e88c2fc494ce49867f54a9c
Joel V Stan <101164781+JoelVStan@users.noreply.github.com>
2022-05-26 20:14:25 +0530

    Update advanced.py

--- advanced.py
+++ advanced.py
@@ -4,24 +4,24 @@ def play():
     user_points = 0
     computer_points = 0
 
-    max_points = int(input("Enter the maximum points:"))
-    while user_points <= max_points or computer_points <= max_points:
-        user = input("Enter yourn choice: rock/paper/scissors\n").lower()
-        computer = random.choice(['rock','paper','scissors'])
+    ## max_points = int(input("Enter the maximum points:"))
+    ## while user_points <= max_points or computer_points <= max_points:
+    user = input("Enter yourn choice: rock/paper/scissors\n").lower()
+    computer = random.choice(['rock','paper','scissors'])
 
-        if user == computer:
-            result = f"\nIt's a tie, computer showed {computer}"
-            return result
+    if user == computer:
+        result = f"\nIt's a tie, computer showed {computer}"
+        return result
 
         # r>s, s>p, p>r
 
-        if is_win(user, computer):
-            result = f"\nYou won, computer showed {computer}"
-            user_points+=1
-            return result
+    if is_win(user, computer):
+        result = f"\nYou won, computer showed {computer}"
+        ## user_points+=1
+        return result
     
-        computer_points +=1
-        return f"\nYou lost, computer showed {computer}"
+    ## computer_points +=1
+    return f"\nYou lost, computer showed {computer}"
     
 
 def is_win(player, opponent):
