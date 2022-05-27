3ee3d9ca9b952354ecfbc3fccd122456d55b412e
Joel V Stan <101164781+JoelVStan@users.noreply.github.com>
2022-05-27 21:43:14 +0530

    Update advanced.py

--- advanced.py
+++ advanced.py
@@ -1,35 +1,44 @@
 import random
 
-def play():
-    user_points = 0
-    computer_points = 0
-
-    ## max_points = int(input("Enter the maximum points:"))
-    ## while user_points <= max_points or computer_points <= max_points:
-    user = input("Enter yourn choice: rock/paper/scissors\n").lower()
-    computer = random.choice(['rock','paper','scissors'])
-
-    if user == computer:
-        result = f"\nIt's a tie, computer showed {computer}"
-        return result
-
-        # r>s, s>p, p>r
-
-    if is_win(user, computer):
-        result = f"\nYou won, computer showed {computer}"
-        ## user_points+=1
-        return result
-    
-    ## computer_points +=1
-    return f"\nYou lost, computer showed {computer}"
-    
-
 def is_win(player, opponent):
     # return true if player wins
     # r>s, s>p, p>r
-    if (player == 'rock' and opponent == 'scissors') or (player == 'scissors' and opponent == 'paper') or (player == 'paper' and opponent == 'rock'):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
 
-if __name__ == '__main__':
+u = 0
+c = 0
 
-    print(play())
+while True:
+    user = input("Enter your choice: rock(r)/paper(p)/scissors(s)\n").lower()
+    computer = random.choice(['r','p','s'])
+
+    if user == computer:
+       print(f"\nIt's a tie, computer also showed {computer}")
+       print(f"\nCurrent Points\nYou - {u}\nComputer - {c}")
+
+    # r>s, s>p, p>r
+
+    elif is_win(user, computer):
+        u = u + 1
+        print(f"\nYou got a point! Computer showed {computer}.")
+        print(f"\nCurrent Points\nYou - {u}\nComputer - {c}")
+    
+    else:
+        c = c + 1
+        print(f"\nOh! Computer showed {computer}. Computer gets a point")
+        print(f"\nCurrent Points\nYou - {u}\nComputer - {c}")
+
+    play_again = input("\nPlay again? (y/n): ")
+    if play_again.lower() != "y":
+        if c>u:
+            print(f"\nGame ends, Computer won! {c} - {u}")
+        elif u>c:
+            print(f"\nGame ends, You won! {u} - {c}")
+        else:
+            print(f"\nGame ends in a tie!! {u} - {c}")
+        break
+
+
+
+    
